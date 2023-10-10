e06cd509db105d3b0c22a5846852b7886776b889
sarcosta33 <sarcosta@hartford.edu>
2023-10-10 16:16:35 -0400

    Added if-else statement for even

--- Diamond.java
+++ Diamond.java
@@ -3,33 +3,59 @@ import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
 
-    int size = 0;
-    int sizeHalf = size/2;
-    int numSpaces;
-    int numStars;
+    int size;
 
     Scanner scan = new Scanner(System.in);
     System.out.print("Enter a number: ");
     size = scan.nextInt();
     scan.close();
 
-    // Calculates number of spaces and stars
-    for(int i = 0; i < size; i++){
-      numSpaces = Math.abs(sizeHalf - i);
-      numStars = 2 * (sizeHalf - numSpaces) + 1;
+    int sizeHalf = size/2;
+
+    // if size enetered is even
+    if(size % 2 == 0){
+
+      // Calculates number of spaces and stars
+      for(int i = 0; i <= size - 1; i++){
+        int numSpaces = Math.abs(sizeHalf - i); //CHANGE FOR EVEN
+        int numStars = 2 * (sizeHalf - numSpaces) + 1;
+
+        // Prints spaces before star
+        for(int a = 0; a < numSpaces; a++){
+          System.out.print(" ");
+        }
+
+        // Prints star on each row
+        for(int b = 0; b < numStars - 1; b++){
+          System.out.print("* ");
+        }
+
+        // Moves to next line
+        System.out.println();
       }
-
-    // Prints spaces before star
-    for(int a = 0; a < numSpaces; a++){
-      System.out.println(" ");
     }
 
-    // Prints star on each row
-    for(int b = 0; b < numStars; b++){
-      System.out.println("*");
-    }
+    // if size entered is odd
+    else{
 
-    // Moves to next line
-    System.out.println();
-    }
+      // Calculates number of spaces and stars
+      for(int i = 0; i < size; i++){
+        int numSpaces = Math.abs(sizeHalf - i);
+        int numStars = 2 * (sizeHalf - numSpaces) + 1;
+
+        // Prints spaces before star
+        for(int a = 0; a < numSpaces; a++){
+          System.out.print(" ");
+        }
+
+        // Prints star on each row
+        for(int b = 0; b < numStars; b++){
+          System.out.print("*");
+        }
+
+        // Moves to next line
+        System.out.println();
+        }
+      }
   }
+}
